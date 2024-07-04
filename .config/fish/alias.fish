7e47d7c97be551c28e66d1aa06f0e1c46e82b1f3
forestion24 <jonasz.ciesielski@lsconsulting.com.pl>
2024-07-04 02:04:24 +0200

    lockscreen
    going to make it better some day

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,4 +1,4 @@
-alias l='eza --long --icons --all --group-directories-first --smart-group'
+alias ls='eza --long --icons --all --group-directories-first --smart-group'
 alias ll='eza --long --icons --all --group-directories-first --smart-group --total-size'
 
 alias gitree='git log --oneline --graph --color --all --decorate'
