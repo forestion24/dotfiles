cde16cc086accbc64075376bc4c1ad16fc477679
forestion24 <jonasz.ciesielski@lsconsulting.com.pl>
2024-07-05 02:40:06 +0200

    logout menu
    added wlogout going to change the config a bit more later

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,4 +1,4 @@
 alias ls='eza --long --icons --all --group-directories-first --smart-group'
-alias ll='eza --long --icons --all --group-directories-first --smart-group --total-size'
+alias ll='eza --long --icons --all --group-directories-first --smart-group --total-size --header'
 
 alias gitree='git log --oneline --graph --color --all --decorate'
